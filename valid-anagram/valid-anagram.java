61d182154408cdc393ce404eac3863d03c8d1d11
Varun Saxena <36344027+vsaxena12@users.noreply.github.com>
2025-05-02 07:21:20 -0500

    Update valid-anagram.java

--- valid-anagram/valid-anagram.java
+++ valid-anagram/valid-anagram.java
@@ -18,3 +18,27 @@ class Solution {
         return true;
     }
 }
+
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if(s.length() != t.length()) {
+//             return false;
+//         }
+//         int[] num = new int[26];
+
+//         for(int i=0; i<s.length(); i++) {
+//             num[s.charAt(i)-'a']++;
+//         }
+
+//         for(int i=0; i<t.length(); i++) {
+//             int element = num[t.charAt(i)-'a']--;
+//         }
+
+//         for(int i: num) {
+//             if(i != 0)
+//                 return false;
+//         }
+
+//         return true;
+//     }
+// }
