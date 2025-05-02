4b0a92e1d04ec0e378a10bffd08215d6ac474b2c
Varun Saxena <36344027+vsaxena12@users.noreply.github.com>
2025-05-01 23:51:22 -0500

    Using Sorting

--- contains-duplicate/contains-duplicate.java
+++ contains-duplicate/contains-duplicate.java
@@ -10,3 +10,16 @@ class Solution {
         return false;
     }
 }
+
+// This is using sorting
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         Arrays.sort(nums);
+
+//         for(int i=0; i<nums.length-1; i++) {
+//             if(nums[i] == nums[i+1])
+//                 return true;
+//         }
+//         return false;
+//     }
+// }
