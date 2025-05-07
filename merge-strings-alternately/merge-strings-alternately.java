c2090d7b9fae4bef2cbfc7e05c6c55ca40c88518
Varun Saxena <36344027+vsaxena12@users.noreply.github.com>
2025-05-06 21:27:24 -0500

    Update merge-strings-alternately.java

--- merge-strings-alternately/merge-strings-alternately.java
+++ merge-strings-alternately/merge-strings-alternately.java
@@ -2,28 +2,47 @@ class Solution {
     public String mergeAlternately(String word1, String word2) {
         
         StringBuilder sb = new StringBuilder();
-        int i = 0;
-        int j = 0;
+        int n = word1.length() + word2.length();
 
-        while(i<word1.length() && j<word2.length()) {
-            if(i<=j) {
+        for(int i=0; i<n; i++) {
+            if(i < word1.length()) {
                 sb = sb.append(word1.charAt(i));
-                i++;
-            } else {
-                sb = sb.append(word2.charAt(j));
-                j++;
             }
-        }
+            if(i < word2.length()) {
+                sb = sb.append(word2.charAt(i));
+            } 
 
-        while(i<word1.length()) {
-            sb = sb.append(word1.charAt(i));
-            i++;
-        }
-
-        while(j<word2.length()) {
-            sb = sb.append(word2.charAt(j));
-            j++;
         }
         return sb.toString();
     }
 }
+
+// class Solution {
+//     public String mergeAlternately(String word1, String word2) {
+        
+//         StringBuilder sb = new StringBuilder();
+//         int i = 0;
+//         int j = 0;
+
+//         while(i<word1.length() && j<word2.length()) {
+//             if(i<=j) {
+//                 sb = sb.append(word1.charAt(i));
+//                 i++;
+//             } else {
+//                 sb = sb.append(word2.charAt(j));
+//                 j++;
+//             }
+//         }
+
+//         while(i<word1.length()) {
+//             sb = sb.append(word1.charAt(i));
+//             i++;
+//         }
+
+//         while(j<word2.length()) {
+//             sb = sb.append(word2.charAt(j));
+//             j++;
+//         }
+//         return sb.toString();
+//     }
+// }
