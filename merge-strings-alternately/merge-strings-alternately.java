1592117c9324c34ea567677db6c32f53abe6f030
Varun Saxena <36344027+vsaxena12@users.noreply.github.com>
2025-05-06 21:21:15 -0500

    Update merge-strings-alternately.java

---
+++ merge-strings-alternately/merge-strings-alternately.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        int j = 0;
+
+        while(i<word1.length() && j<word2.length()) {
+            if(i<=j) {
+                sb = sb.append(word1.charAt(i));
+                i++;
+            } else {
+                sb = sb.append(word2.charAt(j));
+                j++;
+            }
+        }
+
+        while(i<word1.length()) {
+            sb = sb.append(word1.charAt(i));
+            i++;
+        }
+
+        while(j<word2.length()) {
+            sb = sb.append(word2.charAt(j));
+            j++;
+        }
+        return sb.toString();
+    }
+}
