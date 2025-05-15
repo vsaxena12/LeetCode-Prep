e3a9539bc6ede35f53893fbcf72116dff13bee66
Varun Saxena <36344027+vsaxena12@users.noreply.github.com>
2025-05-14 21:17:07 -0500

    Update squares-of-a-sorted-array.java

--- squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,22 +1,12 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
+        //1. square the num
+        //2. Sort ---> O(nlogn) + O(n)
         for(int i=0; i<nums.length; i++) {
-            nums[i] = nums[i] * nums[i];
+            nums[i] = nums[i] * nums[i]; 
         }
 
-        int[] result = new int[nums.length];
-        int left = 0;
-        int right = nums.length-1;
-        int idx = nums.length-1; 
-
-        while(left<=right) {
-            if(nums[left] > nums[right]) {
-                result[idx--] = nums[left++];
-            } else {
-                result[idx--] = nums[right--];
-            }
-        }
-
-        return result;
+        Arrays.sort(nums);
+        return nums;
     }
 }
